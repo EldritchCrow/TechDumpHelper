8bbbe6c0331ff7ca77418193f5a8660dee06ee78
Alex Bozeat <alexboz101@gmail.com>
2018-11-26 13:19:02 -0500

    Created inventory page

---
+++ inventory/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Tech Dump Inventory</title>
+		<link rel="stylesheet" href="../resources/inventory.css">
+	</head>
+	<body>
+		<div id="inventory">
+			<div class="item">
+				<button class="item-delete">&cross;</button>
+				<img class="item-image" src="" width="240" height="135">
+				<div class="item-information">
+					<h3 class="item-title">Title</h3>
+					<div class="item-categories">
+						<div class="item-category">Junk</div>
+						<div class="item-category">Computers</div>
+					</div>
+					<p class="item-description">Description</p>
+				</div>
+			</div>
+			<div class="item">
+				<button class="item-delete">&cross;</button>
+				<img class="item-image" src="" width="240" height="135">
+				<div class="item-information">
+					<h3 class="item-title">Title</h3>
+					<div class="item-categories">
+						<div class="item-category">Junk</div>
+						<div class="item-category">Computers</div>
+					</div>
+					<p class="item-description">Description</p>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
