2e05ba13c5d35f25932b5a96828e116718b6d34d
Alex Bozeat <alexboz101@gmail.com>
2018-11-29 10:41:15 -0500

    Added add inventory form

--- inventory/index.php
+++ inventory/index.php
@@ -5,7 +5,38 @@
 		<link rel="stylesheet" href="../resources/inventory.css">
 	</head>
 	<body>
+		<div id="header">
+			<h1>Inventory</h1>
+		</div>
 		<div id="inventory">
+			<div id="new-item">
+				<button>+ New Item</button>
+				<form>
+					<label>
+						Item Name:
+						<input type="text" name="itemname" required>
+					</label><br>
+					<label>
+						Description<br>
+						<textarea name="description"></textarea><br>
+					</label>
+					Categories:<br>
+					<label class="item-category">
+						<input type="checkbox" name="category" value="junk">
+						Junk
+					</label>
+					<label class="item-category">
+						<input type="checkbox" name="category" value="computers">
+						Computers
+					</label><br>
+					<label>
+						Picture:
+						<input type="file" name="picture">
+					</label><br>
+					<input type="submit" value="Submit">
+					<input type="button" value="Cancel">
+				</form>
+			</div>
 			<div class="item">
 				<button class="item-delete">&cross;</button>
 				<img class="item-image" src="" width="240" height="135">
