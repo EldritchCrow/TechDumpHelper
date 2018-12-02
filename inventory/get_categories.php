e6716d595490402df9e85661ee9c1a50435c511c
Alex Bozeat <alexboz101@gmail.com>
2018-12-01 22:36:46 -0500

    Added PHP script for getting categories from the database

---
+++ inventory/get_categories.php
@@ -0,0 +1,18 @@
+<?php
+
+// Set the proper Content-Type header for a JSON response
+header('Content-Type: application/json');
+
+// Connect to the database
+require 'database.php';
+
+// Get the categories from the database
+try{
+	$categories = $sql->query('SELECT ID AS id, Name AS name FROM inventory_categories')->fetchAll();
+}catch(PDOException $e){
+	http_response_code(500);
+	die(json_encode(['error' => 'Failed to query the database']));
+}
+
+// Return the results in JSON format
+echo json_encode($categories);
